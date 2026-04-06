391bd0b9daa7626898ba867c28b787b842b52308
nour_essam <nouressam1123@gmail.com>
2026-04-06 21:37:53 +0200

    Add Student and Department entities and configure one-to-many relationship

---
+++ SchoolSystem.Domain/Entities/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SchoolSystem.Domain.Entities
+{
+    public class Department
+    {
+        public Department()
+        {
+            Students = new HashSet<Student>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        #region Relation With Student
+        [InverseProperty(nameof(Student.Department))]
+        public ICollection<Student> Students { get; set; }
+        #endregion
+
+    }
+}
---
+++ SchoolSystem.Domain/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SchoolSystem.Domain.Entities
+{
+    public class Student
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(100)]
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+
+        #region Relation With Department
+        public int? DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        [InverseProperty(nameof(Entities.Department.Students))]
+        public Department Department { get; set; }
+        #endregion
+
+    }
+}
